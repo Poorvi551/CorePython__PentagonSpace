2fd37789f6729fe29e53dae8eaee5eeff5b1c461
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-03-24 19:42:30 +0530

    Add files via upload

---
+++ standardwaystudent.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self,name,age,usn):
+        self.name=name
+        self.age=age
+        self.usn=usn
+s1=Student("Shannu",26,420)
+s2=Student("Rahulla",29,840)
+print(s2.age)
+print(s1.usn)
