4ab5c2fa5b6ec266ed43215df402bc182ef4fb2e
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-03-26 22:49:35 +0530

    Add files via upload

---
+++ demoparameters.py
@@ -0,0 +1,8 @@
+class Demo:
+    def add(self,a,b):  # a, b here are Formal Parameter
+        c=a+b
+        print(c)
+d1=Demo()
+x=10
+y=20
+d1.add(x,y)            # here x, y are Actual Parameter
