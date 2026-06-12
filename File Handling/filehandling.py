fad552e612e816c940dfb4c83ada2fba6ec4052a
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-06-12 11:30:05 +0530

    Change file mode from write to append
    Updated file handling to append names to the file instead of overwriting.

--- File Handling/filehandling.py
+++ File Handling/filehandling.py
@@ -1,1 +1,9 @@
-name=input("Enter name:")
+# name=input("Enter a name:")
+# ptr=open("filehandling.txt","w")
+# ptr.write(name)
+# ptr.close()
+
+name=input("Enter a name:")
+ptr=open("filehandling.txt","a")
+ptr.write(name+"\n")
+ptr.close()
