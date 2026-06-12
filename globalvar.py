d025d188290060b37a34bcccb5c76859adbd75dd
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-06-12 12:57:31 +0530

    Update globalvar.py

--- globalvar.py
+++ globalvar.py
@@ -14,3 +14,23 @@ fun1()
 print(a)
 fun2()
 print(a)
+
+# accessing global variable using global
+
+a=100
+def fun1():
+    global a
+    a=10
+    b=20
+    print(a)
+    print(b)
+def fun2():
+    a=15
+    b=25
+    print(a)
+    print(b)
+print(a)
+fun1()
+print(a)
+fun2()
+print(a)
