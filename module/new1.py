6b88d026b7790225a4ed87d753a62850765c0fec
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-06-12 11:44:33 +0530

    Add files via upload

---
+++ module/new1.py
@@ -0,0 +1,15 @@
+from p1 import fun1,fun2
+from p2 import fun1,fun2
+fun1()
+fun2()
+fun1()
+fun2()
+
+#or
+
+import p1
+import p2
+p1.fun1()
+p1.fun2()
+p2.fun1()
+p2.fun2()
---
+++ module/p2.py
@@ -0,0 +1,4 @@
+def fun1():
+    print("Inside fun1 in f2")
+def fun2():
+    print("Inside fun2 in f2")
