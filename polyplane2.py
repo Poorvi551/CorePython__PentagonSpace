9a114d2e7cf17ccef43408cf6fdaa25cbb7fc401
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-06-20 08:02:51 +0530

    Update polyplane2.py

--- polyplane2.py
+++ polyplane2.py
@@ -1,37 +1,29 @@
 class Plane:
     def takeoff(self):
         print("Plane is taking off")
-
-
-def fly(self):
-    print("Plane is flying")
-
-
+    def fly(self):
+        print("Plane is flying")
+        
 class Passenger(Plane):
     def land(self):
         print("Plane is landing")
-
-
+        
 class Cargo(Plane):
     def land(self):
         print("Plane is landing")
-
 
 class Fighter(Plane):
     def land(self):
         print("Plane is landing")
 
-
 p1 = Passenger()
 c1 = Cargo()
 f1 = Fighter()
-
 
 def allowplane(ref):
     ref.takeoff()
     ref.fly()
     ref.land()
-
 
 allowplane(p1)
 allowplane(c1)
